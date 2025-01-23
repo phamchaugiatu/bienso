3939d5569e7292236eb73bbc65e7b234ecb24c92
Phạm Châu Gia Tú <90146975+phamchaugiatu@users.noreply.github.com>
2025-01-23 14:21:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,52 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express(); // Tạo instance của express
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+const port = 3000;
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+  })
+);
+
+app.use(express.static(path.join(__dirname)));
+
+let plateNumber = null;
+
+// Route để phục vụ file HTML
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST']
-}));
-
-app.get('/show/:number/:status', (req, res) => {
-    const plateNumber = req.params.number;
-    const status = req.params.status; // 0: xanh, 1: đỏ
-    
-    io.emit('showPlate', {
-        number: plateNumber,
-        status: status
-    });
-    
-    setTimeout(() => {
-        io.emit('hidePlate');
-    }, 3000);
-    
-    res.json({ success: true });
+app.get("/check", (req, res) => {
+  res.json({ plate: plateNumber });
 });
 
-http.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.get("/show/:number/:status", (req, res) => {
+  const plateNumber = req.params.number;
+  const status = req.params.status; // 0: xanh, 1: đỏ
+
+  io.emit("showPlate", {
+    number: plateNumber,
+    status: status,
+  });
+
+  setTimeout(() => {
+    io.emit("hidePlate");
+  }, 3000);
+
+  res.json({ success: true });
+});
+
+http.listen(port, () => {
+  console.log("Server running on port 3000");
 });
